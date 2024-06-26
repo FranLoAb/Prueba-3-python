7e437076682a16169dae6233bbba5c7f398a2d13
FranLoAb <franci.lopeza@duocuc.cl>
2024-06-26 10:25:57 -0400

    Add files via upload

---
+++ prueba3_005D.py
@@ -0,0 +1,54 @@
+import json
+
+json = 'biblioteca.json'
+autores = 'AutorID'
+Nombre = 'Nombre'
+Nacionalidad = 'Nacionalidad'
+Categoria = 'CategoriaID'
+
+
+def Reportes()
+    
+
+def AgregarAutor():      
+    {
+        "AutorID":  id[len] +1,
+        "Nombre": input('ingrese nombre de autor: '),
+        "Nacionalidad": input('Ingrese nacionalidad del autor: ')
+
+        }
+    autores.append(AgregarAutor)
+        
+    
+#Write
+def EditarAutor():
+       with open (autores.load,'w') as file:
+    
+            Nacionalidad= int(input('ingrese la ID del autor'))
+            autores = input('ingrese nombre del autor')
+                            
+
+
+          
+#Delete
+def EliminarAutor():
+    with open(autores.load,'w') as file:
+         for autores in 'biblioteca.json':
+             
+         {
+            "AutorID": int(input("Ingrese ID de autor a eliminar"))
+             if 'AutorID' == 
+             
+         }
+
+        
+
+
+
+
+#Read   
+def BuscarAutor(): 
+    with open (autores.load,'r') as file:
+     for autores in   json:
+         json.load (file)
+
